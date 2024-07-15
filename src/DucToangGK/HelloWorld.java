323faffbc7790e545ab633a2f3c965d74fb05d5f
Đức Toàn <ductoangk2004@gmail.com>
2024-07-15 21:39:01 +0700

    init project

---
+++ src/DucToangGK/HelloWorld.java
@@ -0,0 +1,10 @@
+package DucToangGK;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+	}
+}
